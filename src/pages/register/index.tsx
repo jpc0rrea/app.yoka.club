5ab799c4f470810969155478b3876ddd94b6ae03
Joao Pedro <joaopedrocorrea14@gmail.com>
2024-01-15 12:09:38 -0300

    fix signup page color

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -183,7 +183,7 @@ export default function Login() {
 
   return (
     <>
-      <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="flex min-h-full flex-col justify-center bg-white py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <Image
             className="mx-auto h-12 w-auto"
