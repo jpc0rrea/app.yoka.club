dca50a23c2943504b39dfe619b4fb35ec87dda8d
Joao Pedro <joaopedrocorrea14@gmail.com>
2024-04-03 08:04:01 -0300

    revert

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -68,20 +68,17 @@ export default class MyDocument extends Document {
           <meta name="description" content="turma de yoga online" />
         </Head>
         <body className="h-full">
-          {/* <!-- Google Tag Manager (noscript) --> */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-TGRV8BZ4"
-              title="google tag manager"
-              height="0"
-              width="0"
-              style={{
-                display: 'none',
-                visibility: 'hidden',
-              }}
-            />
-          </noscript>
-          {/* <!-- End Google Tag Manager (noscript) --> */}
+          {/* <!-- Google Tag Manager --> */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TRSJZMHK');`,
+            }}
+          ></script>
+          {/* <!-- End Google Tag Manager --> */}
           <Main />
           <NextScript />
         </body>
