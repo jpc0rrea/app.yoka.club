afcc046224b480f058b7b42a8b376abfa2056a99
Joao Pedro <joaopedrocorrea14@gmail.com>
2024-10-13 15:44:27 -0300

    fix favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -47,25 +47,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,100..900;1,9..144,100..900&display=swap"
             rel="stylesheet"
           />
-          {/* Updated favicon section */}
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
           <link
             rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
+            type="image/svg+xml"
+            href="/images/yoka-club/yoka-simbolo-roxo.svg"
           />
 
           <link rel="manifest" href="/site.webmanifest" />
