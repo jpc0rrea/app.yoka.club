ff42740a980b067d3377d0e721c8b827eb5bdd87
jpc0rrea <joaopedrocorrea14@gmail.com>
2025-05-23 16:51:24 -0300

    Upgrading typescript version due to errors with mantine on build;

--- src/components/Heart.tsx
+++ src/components/Heart.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import heartImage from "/images/heart.png";
+import { useState } from 'react';
+import heartImage from '/images/heart.png';
 
 function LikeButton() {
   const [likeCount, setLikeCount] = useState(0);
@@ -17,39 +17,39 @@ function LikeButton() {
 
   const styles = {
     app: {
-      height: "100vh",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      flexDirection: "row",
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'row' as const,
     },
     heartBg: {
-      background: liked ? "rgba(255, 192, 200, 0.7)" : "rgba(255, 192, 200, 0)",
-      borderRadius: "50%",
-      height: "60px",
-      width: "60px",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      transition: "all 100ms ease",
-      position: "relative",
-      cursor: "pointer",
+      background: liked ? 'rgba(255, 192, 200, 0.7)' : 'rgba(255, 192, 200, 0)',
+      borderRadius: '50%',
+      height: '60px',
+      width: '60px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      transition: 'all 100ms ease',
+      position: 'relative' as const,
+      cursor: 'pointer',
     },
     heartIcon: {
-      height: "100px",
-      width: "100px",
+      height: '100px',
+      width: '100px',
       backgroundImage: `url(${'/images/heart.png'})`,
-      backgroundSize: "cover",
-      backgroundPosition: liked ? "right" : "left",
-      position: "absolute",
-      animation: liked ? "likeAnim 0.7s steps(28) forwards" : "none",
+      backgroundSize: 'cover',
+      backgroundPosition: liked ? 'right' : 'left',
+      position: 'absolute' as const,
+      animation: liked ? 'likeAnim 0.7s steps(28) forwards' : 'none',
     },
     likeAmount: {
-      fontSize: "20px",
-      fontFamily: "Roboto, sans-serif",
-      color: liked ? "red" : "#888",
-      fontWeight: "900",
-      marginLeft: "6px",
+      fontSize: '20px',
+      fontFamily: 'Roboto, sans-serif',
+      color: liked ? 'red' : '#888',
+      fontWeight: '900',
+      marginLeft: '6px',
     },
     keyframes: `
       @keyframes likeAnim {
